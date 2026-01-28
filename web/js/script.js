af13f70db7a030d11ac936a3adf94b50ce492113
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2026-01-28 16:58:47 +0530

    doo

--- web/js/script.js
+++ web/js/script.js
@@ -7,3 +7,11 @@ function hideMenu(){
     menu.style.left="-350px"
    document.body.style.overflow="auto"
 }
+
+// menu dropdown
+ let iconn =  document.querySelector(".menudropdown i")
+      let sublistt =  document.querySelector(".menudropdown_sublist")
+     function showMe(){
+iconn.classList.toggle("active")
+sublistt.classList.toggle("active")
+     }
